a12d75bde934dddbdcd07d5fb6a8804f33485fb8
rawanatef634 <atron944@gmail.com>
2025-02-27 19:40:16 +0200

    created authentication routes

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,6 +1,10 @@
-import { Request, Response } from "express";
+import { Express, NextFunction, Request, Response } from "express";
 import { config } from "../config/app.config";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
+import { registerSchema } from "../validation/auth.validation";
+import { registerUserService } from "../services/auth.service";
+import { HTTPSTATUS } from "../config/http.config";
+import passport from "passport";
 
 export const googleLoginCallback = asyncHandler(
     async (req: Request, res: Response) => {
@@ -15,5 +19,69 @@ export const googleLoginCallback = asyncHandler(
         return res.redirect(
             `${config.FRONTEND_ORIGIN}/workspace/${currentWorkspace}`
         )
+    }
+)
+export const registerUserController  = asyncHandler(
+    async (req: Request, res: Response) => {
+        const body = registerSchema.parse({
+            ...req.body,
+        });
+
+        await registerUserService(body);
+
+        return res.status(HTTPSTATUS.CREATED).json({
+            message: "User registered successfully",
+        })
+    }
+)
+
+export const loginController  = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        passport.authenticate(
+            "local",
+            (
+                err: Error | null,
+                user: Express.User | false,
+                info: { message: string } | undefined 
+            ) => {
+                if (err) {
+                    return next(err);
+                }
+
+                if (!user) {
+                    return res.status(HTTPSTATUS.UNAUTHORIZED).json({
+                        message: info?.message || "Invalid email or password",
+                    });
+                }
+                req.logIn(user, (err) => {
+                    if (err) {
+                        return next(err);
+                    }
+
+                    return res.status(HTTPSTATUS.OK).json({
+                        message: "User logged in successfully",
+                        user
+                    });
+
+                })
+            }
+        ) (req, res, next)
+    }
+)
+export const logOutController = asyncHandler(
+    async (req: Request, res: Response) => {
+        req.logOut((err) => {
+            if (err) {
+                return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
+                    message: "Failed to log out user",
+                    error: err?.message,
+                });
+            }
+        })
+        req.session = null;
+
+        return res.status(HTTPSTATUS.OK).json({
+            message: "User logged out successfully",
+        })
     }
 )
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,23 +1,35 @@
-import { Router } from 'express'
-import passport from 'passport';
+import { Router } from "express";
+import passport from "passport";
 import { config } from "../config/app.config";
-import { googleLoginCallback } from '../controllers/auth.controller';
-const authRoutes = Router();
+import {
+  googleLoginCallback,
+  loginController,
+  logOutController,
+  registerUserController,
+} from "../controllers/auth.controller";
 
 const failedUrl = `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`;
+
+const authRoutes = Router();
+
+authRoutes.post("/register", registerUserController);
+authRoutes.post("/login", loginController);
+
+authRoutes.post("/logout", logOutController);
+
 authRoutes.get(
-    "/google",
-    passport.authenticate("google", {
-        scope: ["profile", "email"]
-    })
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
 );
 
 authRoutes.get(
-    "/google/callback",
-    passport.authenticate("google", {
-        failureRedirect: failedUrl
-    }),
-    googleLoginCallback
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: failedUrl,
+  }),
+  googleLoginCallback
 );
 
-export default authRoutes
+export default authRoutes;
